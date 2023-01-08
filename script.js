129efa86b4ef8ee3bd04bc327f17fd2305ef13be
Akif Tokic <57287289+akiftokic@users.noreply.github.com>
2023-01-08 15:15:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//ovo je vrijednost kelvina
+let kelvin = "";
+let celsius = kelvin - 273;
+// ovo je vrijednost celsiusa
+let fahrenheit = celsius * (9/5) + 32;
+// ovo je vrijednost farenhajta
+
+// ovo je zaokruzeni farenhajt
+
+
+
+function izracunaj(){
+    let kel = document.getElementById("kelvini").value - 273;
+    let cel = document.getElementById("celzijusi").value = Math.floor(kel);
+
+    
+   
+}
